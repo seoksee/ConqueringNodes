51c48b7dfde6a06478e21dc3a13a1e4894483cc9
Seok See <seoksee@outlook.com>
2020-06-12 19:01:46 +0800

    solved_antialiasing_background_prob_added_timer

--- src/DrawGraph.java
+++ src/DrawGraph.java
@@ -1,22 +1,22 @@
-import javax.sound.sampled.Line;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 
 public class DrawGraph extends JFrame {
     private ArrayList<Node> nodes = new ArrayList<Node>();
     private ArrayList<Edge> edges = new ArrayList<Edge>();
     Random rand = new Random();
-    DecimalFormat format = new DecimalFormat("0.00");
+    int time;
+    long startTime = System.currentTimeMillis();
+    long elapsedTime = 0L;
 
-    public DrawGraph(){
+    public DrawGraph(int time){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.time = time;
     }
 
     public void CreateNodes (int num){
@@ -35,20 +35,13 @@ public class DrawGraph extends JFrame {
                     y=rand.nextFloat()*(max_y-min_y)+min_y;
                 }
             }
-            BigDecimal bd = new BigDecimal(x).setScale(2, RoundingMode.HALF_UP);
-            float x2 = bd.floatValue();
-//            System.out.println(x2);
-//            System.out.println("x: " + roundingFloat(x));
-//            System.out.println("y: " + roundingFloat(y));
             nodes.add(new Node(x, y));
             this.repaint();
         }
     }
 
-
-
     public void AddEdges(){
-        while(edges.size() != Math.floor(nodes.size()/2)){
+        while(elapsedTime< time*1000){
             int i = rand.nextInt(nodes.size());
             if(!nodes.get(i).used){
                 int j = rand.nextInt(nodes.size());
@@ -58,8 +51,9 @@ public class DrawGraph extends JFrame {
                     nodes.get(j).used = true;
                 }
             }
+            elapsedTime = (new Date()).getTime() - startTime;
         }
-
+        System.out.println("Times up!");
         this.repaint();
     }
 
@@ -74,15 +68,13 @@ public class DrawGraph extends JFrame {
 
     public void paint(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
-
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_DEFAULT);
+        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setStroke(new BasicStroke(2));
+        super.paint(g2d);
         for (Node n: nodes){
             Ellipse2D.Float circle = new Ellipse2D.Float((n.x-5),(n.y-5),10f,10f);
-//            g2d.drawOval((int)(n.x), (int)n.y, 15,15);
             g2d.fill(circle);
             System.out.println(n.x + ", " + n.y);
         }
@@ -90,8 +82,7 @@ public class DrawGraph extends JFrame {
         for (Edge e: edges){
             Shape line = new Line2D.Float(nodes.get(e.i).x, nodes.get(e.i).y, nodes.get(e.j).x, nodes.get(e.j).y);
             g2d.draw(line);
-//            g2d.drawLine((int)nodes.get(e.i).x, (int)nodes.get(e.i).y, (int)nodes.get(e.j).x, (int)nodes.get(e.j).y);
+            System.out.println("line " + e.i + " and " + e.j);
         }
-        g2d.setBackground(Color.BLUE);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,14 +10,17 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter n: ");
         int n = input.nextInt();
-        DrawGraph frame = new DrawGraph();
+        System.out.print("Enter t (in seconds): ");
+        int t = input.nextInt();
+        System.out.print("Enter m: ");
+        int m = input.nextInt();
+        DrawGraph frame = new DrawGraph(t);
         frame.setTitle("Game Board");
         frame.setSize(1200,1100);
+        frame.getContentPane().setBackground(Color.WHITE);
         frame.setVisible(true);
         frame.setResizable(false);
-
         frame.CreateNodes(n);
         frame.AddEdges();
-        System.out.println(frame.getBackground());
     }
 }
